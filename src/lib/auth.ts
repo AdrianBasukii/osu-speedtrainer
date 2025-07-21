1ef92b9be47536e80395feb2783ec57a90ccf883
Adrian <adriannugrohobasuki@gmail.com>
2025-07-21 17:21:22 +0700

    push to db

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,10 +1,29 @@
 import NextAuth from "next-auth"
+import User from "@/models/User"
 import Google from "next-auth/providers/google"
+import connectDB from "./db"
 
 export const { auth, handlers, signIn, signOut } = NextAuth({
   providers: [Google],
   callbacks:{
-    session({session, token}){
+    async session({session}){
+      await connectDB()
+      const email = session.user.email
+      const existingUser = await User.findOne({email})
+
+      if(!existingUser){
+        const user = await User.create({
+          name: session.user.name,
+          email: session.user.email,
+          password: "-",
+        })
+
+        
+      }
+      else{
+        console.log("USER EXIST")
+      }
+    
       return session
     }
   }
--- src/models/User.ts
+++ src/models/User.ts
@@ -3,8 +3,7 @@ import mongoose from 'mongoose'
 const userSchema = new mongoose.Schema({
     name: {type: String, required: true},
     email: {type: String, required: true, lowercase: true, unique: true},
-    password: {type: String, required: true},
-    provider: {type: String, required: true}
+    password: {type: String, required: true}
 })
 
 const User = mongoose.models.User || mongoose.model("User", userSchema)
