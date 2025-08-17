3b4bff9294d552d4c656b65ab7a3f636bc5b22b4
Adrian <adriannugrohobasuki@gmail.com>
2025-08-17 19:18:02 +0700

    recent activity responsive design

--- src/app/components/Profile/RecentContainer.tsx
+++ src/app/components/Profile/RecentContainer.tsx
@@ -21,61 +21,114 @@ export default function RecentContainer({recentActivity} : {recentActivity: rece
     }
 
     return (
-        <div>
-            <table className="w-full table-fixed border-separate border-spacing-y-3">
-                <thead className="sticky top-0">
-                    <tr className="text-left h-12">
-                        <th className="bg-bg-secondary border-t-2 border-b-2 border-l-2 rounded-l-xl border-accent-secondary px-4">Consistency</th>
-                        <th className="bg-bg-secondary border-t-2 border-b-2 border-accent-secondary">Mode</th>
-                        <th className="hidden md:table-cell bg-bg-secondary border-t-2 border-b-2 border-accent-secondary">Duration</th>
-                        <th className="hidden md:table-cell bg-bg-secondary border-t-2 border-b-2 border-accent-secondary">Clicks</th>
-                        <th className="bg-bg-secondary border-t-2 border-b-2 border-r-2 md:border-r-0 border-accent-secondary rounded-r-xl md:rounded-none">BPM</th>
-                        <th className="hidden md:table-cell bg-bg-secondary border-t-2 border-b-2 border-r-2 rounded-r-xl border-accent-secondary px-4">Created</th>
-                    </tr>
-                </thead>
+        <div className="w-full overflow-auto">
+            {/* Mobile View*/}
+            <div className="block md:hidden space-y-3">
                 <AnimatePresence>
-                {recentActivity.length>0 ?
-                    selectedActivity.map((data, key) => (
-                    <tbody key={key}>
-                        
-                        <motion.tr 
-                            className="text-left bg-bg-tertiary w-full h-12 mb-4 font-medium"
-                            initial={{ opacity: 0, y: -20 }}
-                            animate={{ opacity: 1, y: 0 }}
-                            transition={{ duration: 0.2}}
-                            exit={{ opacity: 0, y: -20}}
-                        >
-                            <td className="rounded-l-xl px-4">{data.consistency}%</td>
-                            <td>{data.mode}</td>
-                            <td className="hidden md:table-cell" >{data.duration} seconds</td>
-                            <td className="hidden md:table-cell" >{data.clicks} clicks</td>
-                            <td className="rounded-r-xl md:rounded-none">{data.bpm} BPM</td>
-                            <td className="hidden md:table-cell rounded-r-xl px-4 text-sm">{formatDistanceToNow(data.setDate)} ago</td>
-                        </motion.tr>
-                    </tbody>
-                    )) : 
-                    <tbody>
-                        <tr>
-                        <td className="font-medium text-xl text-accent-secondary text-center h-24" colSpan={6}>
-                            No Recent Activity
-                        </td>
-                        </tr>
-                    </tbody>
-                    } 
+                    {recentActivity.length > 0 ?
+                        recentActivity.map((data, key) => (
+                            <motion.div 
+                                key={key}
+                                className="bg-bg-tertiary rounded-xl p-4 flex flex-col gap-4"
+                                initial={{ opacity: 0, y: -20 }}
+                                animate={{ opacity: 1, y: 0 }}
+                                transition={{ duration: 0.2}}
+                                exit={{ opacity: 0, y: -20}}
+                            >
+                                <div className="flex justify-center pb-1 border-b border-accent-secondary/20">
+                                    <span className="text-2xl font-bold">{data.consistency}%</span>
+                                </div>
+                                <div className="w-fit grid grid-cols-2 gap-x-12 gap-y-3 m-auto text-md">
+                                    <div>
+                                        <span className="text-accent-secondary">Mode:</span>
+                                        <div className="font-medium">{data.mode}</div>
+                                    </div>
+                                    <div>
+                                        <span className="text-accent-secondary">BPM:</span>
+                                        <div className="font-medium">{data.bpm} BPM</div>
+                                    </div>
+                                    <div>
+                                        <span className="text-accent-secondary">Duration:</span>
+                                        <div className="font-medium">{data.duration}s</div>
+                                    </div>
+                                    <div>
+                                        <span className="text-accent-secondary">Clicks:</span>
+                                        <div className="font-medium">{data.clicks}</div>
+                                    </div>
+                                </div>
+                                <div className="text-center pt-1 border-t border-accent-secondary/20">
+                                    <span className="text-sm text-accent-secondary font-medium">
+                                        {formatDistanceToNow(data.setDate)} ago
+                                    </span>
+                                </div>
+                            </motion.div>
+                        )) : 
+                        <div className="bg-bg-tertiary rounded-xl p-8 text-center">
+                            <span className="font-medium text-xl text-accent-secondary">
+                                No Recent Activity
+                            </span>
+                        </div>
+                    }
                 </AnimatePresence>
-                
-            </table>
-            {recentActivity.length>5 &&
-            <div className="w-full py-3 flex items-center justify-center">
-                <button onClick={() => handleShow()} className="flex items-center justify-center gap-3 text-sm text-accent-secondary border-2 border-bg-tertiary px-4 py-1 rounded-full hover:cursor-pointer">
-                    {!shown && "Show More"}
-                    {shown && "Show Less"}
-                    <div className={`transition-all ${shown ? 'rotate-180' : ''}`}>
-                        <KeyboardArrowDownIcon/>
-                    </div>
-                </button>
             </div>
-            }
+
+            {/* Desktop View*/}
+            <div className="hidden md:block">
+                <table className="w-full table-fixed border-separate border-spacing-y-3">
+                    <thead className="sticky top-0">
+                        <tr className="text-left h-12">
+                            <th className="bg-bg-secondary border-t-2 border-b-2 border-l-2 rounded-l-xl border-accent-secondary px-4">Consistency</th>
+                            <th className="bg-bg-secondary border-t-2 border-b-2 border-accent-secondary">Mode</th>
+                            <th className="hidden md:table-cell bg-bg-secondary border-t-2 border-b-2 border-accent-secondary">Duration</th>
+                            <th className="hidden md:table-cell bg-bg-secondary border-t-2 border-b-2 border-accent-secondary">Clicks</th>
+                            <th className="bg-bg-secondary border-t-2 border-b-2 border-r-2 md:border-r-0 border-accent-secondary rounded-r-xl md:rounded-none">BPM</th>
+                            <th className="hidden md:table-cell bg-bg-secondary border-t-2 border-b-2 border-r-2 rounded-r-xl border-accent-secondary px-4">Created</th>
+                        </tr>
+                    </thead>
+                    <AnimatePresence>
+                    {recentActivity.length>0 ?
+                        selectedActivity.map((data, key) => (
+                        <tbody key={key}>
+                            
+                            <motion.tr 
+                                className="text-left bg-bg-tertiary w-full h-12 mb-4 font-medium"
+                                initial={{ opacity: 0, y: -20 }}
+                                animate={{ opacity: 1, y: 0 }}
+                                transition={{ duration: 0.2}}
+                                exit={{ opacity: 0, y: -20}}
+                            >
+                                <td className="rounded-l-xl px-4">{data.consistency}%</td>
+                                <td>{data.mode}</td>
+                                <td className="hidden md:table-cell" >{data.duration} seconds</td>
+                                <td className="hidden md:table-cell" >{data.clicks} clicks</td>
+                                <td className="rounded-r-xl md:rounded-none">{data.bpm} BPM</td>
+                                <td className="hidden md:table-cell rounded-r-xl px-4 text-sm">{formatDistanceToNow(data.setDate)} ago</td>
+                            </motion.tr>
+                        </tbody>
+                        )) : 
+                        <tbody>
+                            <tr>
+                            <td className="font-medium text-xl text-accent-secondary text-center h-24" colSpan={6}>
+                                No Recent Activity
+                            </td>
+                            </tr>
+                        </tbody>
+                        } 
+                    </AnimatePresence>
+                    
+                </table>
+                {recentActivity.length>5 &&
+                <div className="w-full py-3 flex items-center justify-center">
+                    <button onClick={() => handleShow()} className="flex items-center justify-center gap-3 text-sm text-accent-secondary border-2 border-bg-tertiary px-4 py-1 rounded-full hover:cursor-pointer">
+                        {!shown && "Show More"}
+                        {shown && "Show Less"}
+                        <div className={`transition-all ${shown ? 'rotate-180' : ''}`}>
+                            <KeyboardArrowDownIcon/>
+                        </div>
+                    </button>
+                </div>
+                }
+            </div>
         </div>
     );
 }
